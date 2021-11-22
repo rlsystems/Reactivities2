979bc095b91294cb1a4b2bb20005a9fa0634846f
rlsystems <rlsystems@outlook.com>
2021-11-22 12:13:26 -0500

    End of section 6

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,54 @@
+import React, { SyntheticEvent, useState } from 'react';
+import { Button, Item, Label, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+
+interface Props {
+    activities: Activity[];
+    selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
+    submitting: boolean;
+}
+
+export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
+    const [target, setTarget] = useState('');
+
+    //Could use Any as the type for the click event, or use SyntheticEvent for super specific
+    function handleActivityDelete (e: SyntheticEvent<HTMLButtonElement>, id: string) {
+        setTarget(e.currentTarget.name) //e.target.name for 'any' type
+        deleteActivity(id)
+    }
+
+    return (
+        <Segment>
+            <Item.Group divided>
+                {activities.map(activity => (
+                    <Item key={activity.id}>
+                        <Item.Content>
+                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Meta>{activity.date}</Item.Meta>
+                            <Item.Description>
+                                <div>{activity.description}</div>
+                                <div>{activity.city}, {activity.venue}</div>
+                            </Item.Description>
+                            <Item.Extra>
+                                <Button
+                                    onClick={() => selectActivity(activity.id)}
+                                    floated='right'
+                                    content='View'
+                                    color='blue' />
+                                <Button
+                                    name={activity.id}
+                                    loading={submitting && target === activity.id}
+                                    onClick={(e) => handleActivityDelete(e, activity.id)} //i guess that with onClick, first param is always the event
+                                    floated='right'
+                                    content='Delete'
+                                    color='red' />
+                                <Label basic content={activity.category} />
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                ))}
+            </Item.Group>
+        </Segment>
+    )
+}
